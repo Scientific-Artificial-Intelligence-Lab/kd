fd704f9836ed97556c7750b62c16cb7f136f6182
vixues <vixues@gmail.com>
2025-01-15 10:55:58 +0800

    feat: Add __all__ list for equation modules

--- kd/dataset/__init__.py
+++ kd/dataset/__init__.py
@@ -1,1 +1,21 @@
-__all__ = []
+__all__ = [
+    "Burgers_equation",
+    "Burgers_equation_shock",
+    "KdV_equation",
+    "KdV_equation_sine",
+    "Chaffee_Infante_equation",
+    "KG_equation",
+    "Allen_Cahn_equation",
+    "Convection_diffusion_equation_solution",
+    "Convection_diffusion_equation_simulation",
+    "Wave_equation",
+    "KS_equation",
+    "Beam_equation",
+    "Heat_equation",
+    "Heat_equation_sin",
+    "Diffusion_equation",
+    "Parametric_convection_diffusion",
+    "Parametric_Burgers_equation",
+    "Parametric_wave_equation",
+    "Burgers_2D"
+]
