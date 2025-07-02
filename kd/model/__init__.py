cdf9eef5c008a38d7534a9fe7bfa69c42d4a47aa
mao-hao <mao_hao@outlook.com>
2025-07-01 22:50:15 -0400

    update

--- kd/model/__init__.py
+++ kd/model/__init__.py
@@ -1,6 +1,7 @@
 """Model implementations for PDE discovery."""
 
 from .dlga import DLGA
+from .kd_dlga import KD_DLGA
 from .deeprl import KD_DSCV, KD_DSCV_Pinn
 
-__all__ = ['DLGA', 'KD_DSCV', 'KD_DSCV_Pinn']
+__all__ = ['DLGA', 'KD_DLGA', 'KD_DSCV', 'KD_DSCV_Pinn']
