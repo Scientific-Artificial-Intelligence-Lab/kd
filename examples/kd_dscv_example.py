84b702470005cf391568ebe8189c0e61d7d7538d
mao-hao <mao_hao@outlook.com>
2025-07-02 23:34:39 -0400

    rename

--- examples/kd_dscv_example.py
+++ examples/kd_dscv_example.py
@@ -22,31 +22,7 @@ model = KD_DSCV(
 )
 
 np.random.seed(42)
-def prepare_data():
-    
-    data = scipy.io.loadmat('./kd/data_file/burgers2.mat')
-    t = np.real(data['t'].flatten()[:,None])
-    x = np.real(data['x'].flatten()[:,None])
-    Exact = np.real(data['usol']).T  # t first
-    X, T = np.meshgrid(x,t)
 
-    X_star = np.hstack((X.flatten()[:,None], T.flatten()[:,None]))
-    u_star = Exact.flatten()[:,None]              
-
-    # Doman bounds
-    lb = X_star.min(0)
-    ub = X_star.max(0) 
-
-    x_len = len(x)
-    total_num = X_star.shape[0]
-    sample_num = int(total_num*0.1)
-    print(f"random sample number: {sample_num} ")
-    ID = np.random.choice(total_num, sample_num, replace = False)
-    X_u_meas = X_star[ID,:]
-    u_meas = u_star[ID,:]
-    return X_u_meas,u_meas, lb,ub
-
-x,y,lb,ub = prepare_data()
 model.import_inner_data(dataset='Burgers', data_type='regular')
 step_output = model.train(n_epochs=51)
 print(f"Current best expression is {step_output['expression']} and its reward is {step_output['r']}")
--- kd/model/__init__.py
+++ kd/model/__init__.py
@@ -2,6 +2,6 @@
 
 from .dlga import DLGA
 from .kd_dlga import KD_DLGA
-from .deeprl import KD_DSCV, KD_DSCV_Pinn
+from .kd_dscv import KD_DSCV, KD_DSCV_Pinn
 
 __all__ = ['DLGA', 'KD_DLGA', 'KD_DSCV', 'KD_DSCV_Pinn']
